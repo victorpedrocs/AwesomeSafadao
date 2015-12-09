f9da02a841139dd512b7b64187254dd218411ff3
Victor Pedro <vpcx5@yahoo.com.br>
2015-12-09 16:05:54 -0200

    fixing copy/paste

--- Python/safadao.py
+++ Python/safadao.py
@@ -3,14 +3,11 @@
 #
 
 def somatorio(mes):
-    sum = 0
-    for i in range(1,mes+1):
-        sum += i
-    return sum
+    return sum( range( 1, mes + 1 ) )
 
 def wesley_safadao(dia, mes, ano):
-    safadeza = somatorio(mes) + (ano/100) * (50-dia);
-    anjo = 100.0 - safadeza;
+    safadeza = somatorio(mes) + (ano/100) * (50-dia)
+    anjo = 100.0 - safadeza
 
     print anjo, "%% ANJO, mas o", safadeza, "%% eh VAGABUNDO"
 
