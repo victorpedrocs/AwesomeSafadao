3fb4965acdf92344d8ceebbfe3d39750d2b5cea8
vpcsilva <vpcx5@yahoo.com.br>
2015-12-09 15:07:07 -0200

    Implementando em C

--- C/safadao.c
+++ C/safadao.c
@@ -2,20 +2,26 @@
 
 int somatorio(int mes) {
     int i, soma = 0;
+
     for (i = 1; i <= mes; i++) {
         soma += i;
     }
+
     return soma;
 }
 
 void wesley_safadao(int dia, int mes, int ano) {
     float safadeza, anjo;
+
     safadeza = somatorio(mes) + (ano/100) * (50-dia);
     anjo = 100 - safadeza;
+
     printf("%.1f %% ANJO, mas o %.1f %% eh VAGABUNDO!\n", anjo, safadeza);
 }
+
 int main(int argc, char const *argv[]) {
     int dia = 0, mes = 0, ano = 0;
+
     printf("Insira sua data de nascimento:\n");
     printf("Dia (dd): ");
     fflush(stdin);
